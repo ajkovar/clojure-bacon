149d74e02a982a080a1cbc59c338e3dca58d5109
Alex Kovar <ajkovar@gmail.com>
2014-11-28 01:43:31 -0600

    initial/hacky version of resistance

--- src/cljs/test/core.cljs
+++ src/cljs/test/core.cljs
@@ -8,14 +8,24 @@
 (defn main []
   (let [key-down-stream (.asEventStream (js/$ js/document) "keydown")
         key-code-stream (.map key-down-stream (fn [e] (.-keyCode e)))
-        acceleration-stream (.map key-code-stream (fn [keyCode] (case keyCode
-                                                                 37 [-1 0]
-                                                                 38 [0 1]
-                                                                 39 [1 0]
-                                                                 40 [0 -1])))
+        velocity-atom (atom {})
+        resistance #(* (/ % 10) -1)
+        keyboard-acceleration-stream (.map key-code-stream (fn [keyCode] (case keyCode
+                                                                          37 [-1 0]
+                                                                          38 [0 1]
+                                                                          39 [1 0]
+                                                                          40 [0 -1]
+                                                                          [0 0])))
+        resistance-acceleration-stream (.flatMapFirst
+                                        (.once js/Bacon 0)
+                                        (fn []
+                                          (.sampledBy (.map @velocity-atom #(map resistance %))
+                                                      (.interval js/Bacon 1000))))
+        acceleration-stream (.merge keyboard-acceleration-stream resistance-acceleration-stream)
         initial-velocity [0, 0]
         velocity-stream (.scan acceleration-stream initial-velocity #(map + %1 %2))
         style {:backgroundColor "blue"}]
+    (swap! velocity-atom (fn [] velocity-stream))
     (.onValue velocity-stream (fn [k] (.log js/console (clj->js k))))
     (om/root
      (fn [app owner]
